847bc52415ffedf0f8b84565c61b797a00055bca
Raul Gabriel <raul.stoianof@cirvianum.cat>
2024-10-16 12:17:19 +0200

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./**/*.html", // O qualsevol ruta a arxius HTML que tinguis
+    "./src/**/*.{js,jsx,ts,tsx}", // Si tens fitxers JS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
